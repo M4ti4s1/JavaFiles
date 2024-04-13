3b5acadd77602ebe9219d742c651cab120cc384b
M4ti4s1 <m4ti4s.404@gmail.com>
2024-04-13 16:16:17 -0400

    Update

--- HomeWork/Vencimiento.java
+++ HomeWork/Vencimiento.java
@@ -3,17 +3,32 @@ import java.util.Scanner;
 public class Vencimiento {
     public static void main(String[] args){
 
-        int dia, mes, anio;
+        int dia, mes, anio, diaVen, mesVen, anioVen;
+        String medicamento;
         boolean vencido;
         Scanner sc = new Scanner(System.in);
+        
+        medicamento = sc.next();
 
-        dia = 15;
-        mes = 04;
-        anio = 2024;
+        // Ingresar fecha Vencimiento
+        diaVen = sc.nextInt();
+        mesVen = sc.nextInt();
+        anioVen = sc.nextInt();
 
-        vencido = (anio < 2024) || (anio == 2024 && mes < 04) || (anio == 2024 && mes == 04 && dia < 13);
+        // Ingresar fecha a consultar
+        dia = sc.nextInt();
+        mes = sc.nextInt();
+        anio = sc.nextInt();
+
+        // Escribir 
+        System.out.println( "Nombre medicamento: " + medicamento);
+        System.out.println( "Fecha Vencimiento: " + diaVen +"-"+mesVen+"-"+anioVen);
+        System.out.println( "Fecha Consulta: " + dia +"-"+mes+"-"+anio);
+
+
+        vencido = (anio < anioVen) || (anio == anioVen && mes < mesVen) || (anio == anioVen && mes == mesVen && dia < diaVen);
         System.out.println(vencido);
-
+        sc.close();
     }
     
 }
