e0962f4c310713292173d50cb10bd70e1bebd51b
M4ti4s1 <m4ti4s.404@gmail.com>
2024-04-23 10:22:42 -0400

    New project

---
+++ MyProjects/BankingSystem/Costumer.java
@@ -0,0 +1,12 @@
+public class Costumer{
+    private int account;
+    private String name;
+    private String adress;
+
+    public Costumer(int account, String name, String adress){
+        this.account = account;
+        this.name = name;
+        this.adress = adress;
+    }
+    class Acount()
+}
