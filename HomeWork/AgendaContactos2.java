5917e3edd2da59a1d2968c7dfc96df81eb00b7c8
M4ti4s1 <m4ti4s.404@gmail.com>
2024-06-13 13:01:34 -0400

    Finishing verion 2 of Contacts agenda

--- HomeWork/AgendaContactos2.java
+++ HomeWork/AgendaContactos2.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class AgendaContactos2{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String[][] agenda = new String[10][10];
+        int agendaLimit = 10;
+        String[][] agenda = new String[agendaLimit][4];
         String[] regiones = {
             "Arica y Parinacota",
             "Tarapaca",
@@ -24,7 +25,7 @@ public class AgendaContactos2{
         };
         int cantContactos=0, opc;
         String find, nameRemove, email="", telefono="", nombre="", region=""; 
-        int contactoRegion, indexRemove=-1;
+        int contactoRegion, indexRemove=-1, indexInsert;
 
         // Ciclo Menu
         do{
@@ -37,15 +38,87 @@ public class AgendaContactos2{
 
             switch(opc){
                 case 1:
-                    agenda[cantContactos][0] = sc.next();
-                    agenda[cantContactos][1] = sc.next();
-                    agenda[cantContactos][2] = sc.next();
-                    // region
-                    contactoRegion = sc.nextInt();
-                    agenda[cantContactos][3] = regiones[contactoRegion]; 
-                    cantContactos++;                    
+                    
+                
+                     
+                    if (cantContactos < agendaLimit){
+                        
+
+                        if (cantContactos == 0){
+                            agenda[cantContactos][0] = sc.next();
+                            agenda[cantContactos][1] = sc.next();
+                            agenda[cantContactos][2] = sc.next();
+                            contactoRegion = sc.nextInt();
+                            agenda[cantContactos][3] = regiones[contactoRegion];
+                            cantContactos++;
+
+                        }else if(cantContactos == 1){
+                            nombre = sc.next();
+                            email = sc.next();
+                            telefono = sc.next();
+                            contactoRegion = sc.nextInt();
+
+                            if (nombre.compareTo(agenda[0][0]) < 0){
+                                indexInsert = 0;
+                            }else{
+                                indexInsert = cantContactos;
+                            }                                
+                            
+
+                            for (int i = cantContactos; i >= indexInsert; i--){
+                                agenda[i+1][0] = agenda[i][0];
+                                agenda[i+1][1] = agenda[i][1];
+                                agenda[i+1][2] = agenda[i][2];
+                                agenda[i+1][3] = agenda[i][3];
+                            }
+                            agenda[indexInsert][0] = nombre;
+                            agenda[indexInsert][1] = email;
+                            agenda[indexInsert][2] = telefono;
+                            agenda[indexInsert][3] = regiones[contactoRegion];
+                            cantContactos++;
+
+                        }else{
+                            nombre = sc.next();
+                            email = sc.next();
+                            telefono = sc.next();
+                            contactoRegion = sc.nextInt();
+
+                            indexInsert = cantContactos;
+
+                            for (int i = cantContactos-1; i >= 0; i--){
+                                
+                                if (nombre.compareTo(agenda[i][0]) < 0){
+                                    indexInsert = i;
+                                    System.out.println(indexInsert);
+                                }
+                            }
+
+                            // voy bien
+
+                            for (int i = cantContactos; i >= indexInsert; i--){
+                                agenda[i+1][0] = agenda[i][0];
+                                agenda[i+1][1] = agenda[i][1];
+                                agenda[i+1][2] = agenda[i][2];
+                                agenda[i+1][3] = agenda[i][3];
+                            }
+                            agenda[indexInsert][0] = nombre;
+                            agenda[indexInsert][1] = email;
+                            agenda[indexInsert][2] = telefono;
+                            agenda[indexInsert][3] = regiones[contactoRegion];
+                            cantContactos++;
+                        }
+
+
+                    }else{
+                        System.out.println("Se alcanzo el maximo de contactos");
+                        System.out.println("Borra uno para ingresar uno nuevo...");
+                    }
+                    
                     break;
+
+
                 case 2:
+                    
                     nameRemove = sc.next();
                     for (int i = 0; i < cantContactos; i++) {
                         if(nameRemove.equals(agenda[i][0])){
@@ -60,16 +133,8 @@ public class AgendaContactos2{
                         agenda[i][2] = agenda[i+1][2];
                         agenda[i][3] = agenda[i+1][3];
 
-                    }
-                    // show the new contacts list 
-                    /* 
-                    for (int i = 0; i < cantContactos-1; i++){
-                        System.out.println(agenda[i][0]);
-                        System.out.println(agenda[i][1]);
-                        System.out.println(agenda[i][2]);
-                        System.out.println(agenda[i][3]);
-                    }
-                    */
+                    } 
+
                     break;
                 
                 case 3:
@@ -96,9 +161,19 @@ public class AgendaContactos2{
                 default:
                     System.out.println("Opcion invalida");
             }
-        }while(opc != 3);
-        
+        }while(opc != 4);
         
 
+
+
+        // escribir la lista de contactos al final del programa
+        for (int i = 0; i < cantContactos; i++){
+            for (int j = 0; j < agenda[0].length; j++){
+                System.out.print(agenda[i][j] + " ");
+            }
+            System.out.println();
+
+        }
+        sc.close();
     }
 }
